24ea8c0fd0fba3c447af87d4bf3387afd0efdb02
Gabriel Morais <greis9010@gmail.com>
2024-12-09 20:43:11 -0300

    chore: Atualiza email de validação do login

--- src/app/actions/login-admin.ts
+++ src/app/actions/login-admin.ts
@@ -1,8 +1,13 @@
 import type { LoginFormData } from "../admin/_components/login-form";
 
 export async function loginAdminAction({ username, password }: LoginFormData) {
-  if (username == "greis9010@gmail.com" && password == "@dexter") {
-    return { authenticate: true };
+  if (
+    username == "greis9010@gmail.com" ||
+    username == "luannacosta.009@gmail.com"
+  ) {
+    if (password == "@dexter") {
+      return { authenticate: true };
+    }
   }
 
   return { authenticate: false };
