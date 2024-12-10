04603b6d2aab60b80e4d209844fa4ffd5cbfa2a7
Gabriel Morais <greis9010@gmail.com>
2024-12-10 08:52:03 -0300

    chore: adiciona formatação lowercase no username

--- src/app/actions/login-admin.ts
+++ src/app/actions/login-admin.ts
@@ -2,8 +2,8 @@ import type { LoginFormData } from "../admin/_components/login-form";
 
 export async function loginAdminAction({ username, password }: LoginFormData) {
   if (
-    username == "greis9010@gmail.com" ||
-    username == "luannacosta.009@gmail.com"
+    username.toLocaleLowerCase() == "greis9010@gmail.com" ||
+    username.toLocaleLowerCase() == "luannacosta.009@gmail.com"
   ) {
     if (password == "@dexter") {
       return { authenticate: true };
